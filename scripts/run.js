1629b9b266e60777e34eae2769ee5ee2be7377bf
scirocco21 <sgz40741@gmail.com>
2021-12-11 22:21:38 -0500

    refac: call contract func in main()

--- scripts/run.js
+++ scripts/run.js
@@ -3,15 +3,25 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
-  };
-  
-  const runMain = async () => {
+
+    // Call the function.
+    let txn = await nftContract.makeAnEpicNFT()
+    // Wait for it to be mined.
+    await txn.wait()
+
+    // Mint another NFT for fun.
+    txn = await nftContract.makeAnEpicNFT()
+    // Wait for it to be mined.
+    await txn.wait()
+};
+    
+const runMain = async () => {
     try {
-      await main();
-      process.exit(0);
+    await main();
+    process.exit(0);
     } catch (error) {
-      console.log(error);
-      process.exit(1);
+    console.log(error);
+    process.exit(1);
     }
   };
   
